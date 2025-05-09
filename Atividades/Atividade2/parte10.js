00b0385b289633f870f97a6dbbbe5d17b33b43fc
JpNery_Tech <jppereiranery@gmail.com>
2025-05-09 17:48:12 -0300

    Delete Atividades directory

--- Atividades/Atividade2/parte10.js
+++
@@ -1,4 +0,0 @@
-const prompt = require('prompt-sync')({ sigint: true });
-let palavra = prompt("Digite uma palavra:");
-let palavraInvertida = palavra.split("").reverse().join("");
-console.log(`A palavra invertida é: ${palavraInvertida}`);
